6f3d3be5b749a5a22479679d80f3ab31fec3c238
Ghost <danyfox1.dm@gmail.com>
2023-10-10 20:09:35 -0300

    showing the moves for knight

--- lib/main.rb
+++ lib/main.rb
@@ -12,10 +12,10 @@ moves = path_for.knight_moves([1, 4], [4, 7])
 puts "\nknight_moves([1, 4], [4, 7]): "
 puts "=> You made it in #{moves.size-1} moves!  Here's your path: #{moves}"
 
-puts "\nUpdate Board from ['d', 1] =>#{knight.white} to ['g', 4] =>#{board.cyan_dot}...\n\n"
-board.update_square('d', 1).change_piece = knight.black 
-board.update_square('e',3).change_piece = board.red_dot
-board.update_square('g', 4).change_piece = board.cyan_dot
+puts "\nUpdate Board from ['e', 1] :#{knight.white}=> ['f', 3] :#{board.red_dot}=> ['h', 4] :#{board.blue_dot}\n\n"
+board.update_square('e', 1).change_piece = knight.black 
+board.update_square('f',3).change_piece = board.red_dot
+board.update_square('h', 4).change_piece = board.blue_dot
 board.show
 
 
@@ -24,9 +24,9 @@ moves_two = path_for.knight_moves([3, 3], [4, 3])
 puts "\nknight_moves([3, 3],[4, 3])"
 puts "=> YOu made it in #{moves_two.size-1} moves!  Here's your path: #{moves_two}"
 
-puts "\nUpdate Board form knight_moves(['d', 3] =>#{knight.white}, ['d', 4] =>#{board.cyan_dot})\n\n"
+puts "\nUpdate Board form knight_moves(['d', 3] :#{knight.white}=> ['e', 5]: #{board.red_dot}=> ['f', 3] :#{board.red_dot}=> ['d', 4] :#{board.blue_dot}\n\n"
 board.update_square('d', 3).change_piece = knight.black 
 board.update_square('e', 5).change_piece = board.red_dot 
 board.update_square('f', 3).change_piece = board.red_dot 
-board.update_square('d', 4).change_piece = board.cyan_dot 
+board.update_square('d', 4).change_piece = board.blue_dot 
 board.show 
