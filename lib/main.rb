30284f75fdb1113924e69c5badd55dfc4fb692aa
Ghost <danyfox1.dm@gmail.com>
2023-10-11 08:20:28 -0300

    cleaning up the main class

--- lib/main.rb
+++ lib/main.rb
@@ -9,30 +9,20 @@ knight = Knight.new
 path_for = SearchMoves.new
 
 moves = path_for.knight_moves([1, 4], [4, 7]) 
-column_row = "# => knight_moves([row, column], [row, column]) row: from 1 to 8 column: 0 to 7('a' to 'h')"
-puts "\nknight_moves([1, 4], [4, 7]):".yellow 
-puts column_row.green 
+puts "\n# => knight_moves([row, column], [row, column]) row: from 1 to 8 column: 0 to 7('a' to 'h')".green
 board.you_made_it(moves)
-
 puts "Update Board from ['e', 1] :#{knight.white}=> ['f', 3] :#{board.red_dot}=> ['h', 4] :#{board.blue_dot}\n\n"
-board.update_square('e', 1).change_piece = knight.black 
-board.update_square('f',3).change_piece = board.red_dot
-board.update_square('h', 4).change_piece = board.blue_dot
-board.show
+board.show_moves_path(moves, knight)
 
 
-board.create_board # reset board 
+board.create_board # reset board board two
 moves_two = path_for.knight_moves([3, 3], [4, 3])
-puts "\nknight_moves([3, 3],[4, 3])".yellow
 board.you_made_it(moves_two)
-
 puts "Update Board form knight_moves(['d', 3] :#{knight.white}=> ['e', 5]: #{board.red_dot}=> ['f', 3] :#{board.red_dot}=> ['d', 4] :#{board.blue_dot}\n\n"
-board.update_square('d', 3).change_piece = knight.black 
-board.update_square('e', 5).change_piece = board.red_dot 
-board.update_square('f', 3).change_piece = board.red_dot 
-board.update_square('d', 4).change_piece = board.blue_dot 
-board.show 
+board.show_moves_path(moves_two, knight)
 
-board.create_board # reset board 
+
+board.create_board # reset board board three
 another_path = path_for.knight_moves([1, 0], [7, 7])
-board.show_moves_path([[1, 0], [3, 1], [5, 2], [7, 3], [8, 5], [7, 7]], knight)
+board.show_moves_path(another_path, knight)
+
